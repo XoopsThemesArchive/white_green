6357bfb5875a17cf36e7152060a585153c51c891
mambax7 <mambax7@gmail.com>
2015-05-04 05:50:45 -0400

    Original files

---
+++ white_green/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "white_green";
+	$txc_themeinfo["author"] = "psycn";
+	$txc_themeinfo["author_url"] = "http://www.psycn.net/";
+	$txc_themeinfo["detail_url"] = "http://ffxk.com/modules/wordpress/255.htm";
+	$txc_themeinfo["download_url"] = "http://ffxk.com/modules/down/";
+?>
